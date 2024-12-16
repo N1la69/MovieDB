036d4fc49adf3d58f52133b0c79d2b88b460cd6f
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2024-12-16 13:54:32 +0530

    Initial Commits

---
+++ client/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View className="flex justify-center items-center">
+      <Text className="text-2xl text-red-400">Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
